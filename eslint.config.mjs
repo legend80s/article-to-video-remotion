8d7ca93bef81bf3709281fd418932dc79e4345b7
legend80s <liuchzong@qq.com>
2026-03-04 20:16:50 +0800

    fix: eslint NPE

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,21 @@ import { config } from "@remotion/eslint-config-flat"
 console.log(config)
 
 // ignore no-unused-vars rule
-config.rules["@typescript-eslint/no-unused-vars"] = [
-  "error",
-  {
-    args: "all",
-    argsIgnorePattern: "^_",
-    caughtErrors: "all",
-    caughtErrorsIgnorePattern: "^_",
-    destructuredArrayIgnorePattern: "^_",
-    varsIgnorePattern: "^_",
-    ignoreRestSiblings: true,
+config.push({
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
   },
-]
+})
 
 export default config
