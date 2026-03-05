36bd489a486974e48a08f86010ce17ab579cd3b0
legend80s <liuchzong@qq.com>
2026-03-05 09:44:24 +0800

    feat(react-height): readme

---
+++ src/articles/react-stars-height/data/landmark.ts
@@ -0,0 +1,22 @@
+type ILandmark = {
+  name: string
+  height: number
+}
+
+export const landmarks: ILandmark[] = [
+  { name: "7层住宅楼", height: 22 },
+  { name: "上海国际商贸中心T2", height: 370 },
+  { name: "上海中心大厦", height: 632 },
+  { name: "哈利法塔", height: 828 },
+  { name: "泰山", height: 1545 },
+  { name: "富士山", height: 3776 },
+  { name: "玉龙雪山", height: 5596 },
+  { name: "念青唐古拉山", height: 7162 },
+  { name: "乔戈里峰", height: 8611 },
+  { name: "珠穆朗玛峰", height: 8848 },
+  { name: "民航客机", height: 10000 },
+  { name: "平流层顶", height: 50000 },
+  { name: "中间层顶", height: 85000 },
+  { name: "卡门线（太空边界）", height: 100000 },
+  { name: "低地球轨道卫星", height: 200000 },
+]
